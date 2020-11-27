58bd7e937ca2d71f134e1cdecab859047b5b7b70
kowshik1632 <69100777+kowshik1632@users.noreply.github.com>
2020-11-27 19:05:26 +0530

    Update connect.py

--- Web Scrapping/connect.py
+++ Web Scrapping/connect.py
@@ -5,13 +5,11 @@ def connect(dbname):
     conn.execute("CREATE TABLE IF NOT EXISTS OYO_HOTELS (NAME TEXT, ADDRESS TEXT, PRICE INT, AMENITIES TEXT, RATING TEXT)")
 
     PRINT("Taple created successfully!")
-
     conn.close()
 
 def insert_into_table(dbname, values):
     conn = sqlite3.connect(dbname)
     insert_sql = "INSERT INTO OYO_HOTELS (NAME, ADDRESS, PRICE, AMENITIES, RATING) VALUES (?, ?, ?, ?, ?)"
-
     conn.execute(insert_sql, values)
 
     CONN.COMMIT()
@@ -19,13 +17,10 @@ def insert_into_table(dbname, values):
 
 def get_hotel_info(dbname):
     conn = sqlite3.connect(dbname)
-
     cur = conn.cursor()
-
     cur.execute("SELECT * FROM OYO_HOTELS")
 
     table_data = cur.fetchall()
-
     for record in table_data:
         print(record)
 
